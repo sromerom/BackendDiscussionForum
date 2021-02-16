700f2823ca6752239e1710251b1182762e07540d
Samuel Romero Marin <sromerom@esliceu.net>
2021-02-16 22:42:20 +0100

    Initial structure

---
+++ src/test/java/com/liceu/sromerom/discussionforum/DiscussionforumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liceu.sromerom.discussionforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscussionforumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
